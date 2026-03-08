964da4118a1b4e8efe5ccea4389017fd1ea86f55
Laiba Aslam <anayachaudry890@gmail.com>
2026-03-08 20:21:44 +0500

    update

--- Task2.cpp
+++ Task2.cpp
@@ -18,17 +18,35 @@ public:
         balance = 0;
     }
 
-    // Setters
-    void setName(string name) { 
-        accountHolderName = name;
-     }
-    void setAccountNumber(int accNum) { 
-        accountNumber = accNum; 
-    }
-    void setBalance(double bal) {
-         balance = bal; 
+    // Setter for name
+    bool setName(string name) { 
+        if(name.empty())
+        {
+            cout<<"Name can not be empty:";
+            return false;
         }
-
+        accountHolderName = name;
+        return true;
+     }
+     // Setter for account number
+    bool setAccountNumber(int accNum) { 
+  if(accNum >= 10000 && accNum <= 99999){
+     accountNumber = accNum; 
+     return true;
+  }       
+        cout << "Invalid Account Number. Must be 5 digits. Try again.\n";
+        return false;
+    }
+    //setter for balance
+   bool setBalance(double bal) {
+    if(bal>=0)
+        {
+             balance = bal; 
+             return true;
+        }
+        cout << "Invalid Balance. Cannot be negative. Try again.\n";
+             return false;
+    }
     // Display Function
     void displayAccountInfo()
     {
@@ -46,49 +64,34 @@ int main()
     int accNum;
     double bal;
 
-    // Input Name with validation
+    // Input Name 
     while(true)
     {
         cout << "Enter Account Holder Name: ";
         getline(cin, name);
-        if(name.empty())
-            cout << "Name cannot be empty. Please enter again.\n";
-        else
-        {
-            acc1.setName(name);
+        if(acc1.setName(name))
             break;
         }
-    }
 
-    // Input Account Number with validation
+    // Input Account Number with 
     while(true)
     {
         cout << "Enter Account Number (5 digits): ";
         cin >> accNum;
 
-        if(accNum >= 10000 && accNum <= 99999)
-        {
-            acc1.setAccountNumber(accNum);
+           if( acc1.setAccountNumber(accNum))
             break;
-        }
-        else
-            cout << "Invalid Account Number. Must be 5 digits. Try again.\n";
     }
 
-    // Input Balance with validation
+    // Input Balance 
     while(true)
     {
         cout << "Enter Balance: ";
         cin >> bal;
 
-        if(bal >= 0)
-        {
-            acc1.setBalance(bal);
-            break;
+        if(acc1.setBalance(bal))
+       break;
         }
-        else
-            cout << "Invalid Balance. Cannot be negative. Try again.\n";
-    }
 
     // Display Account Info
     acc1.displayAccountInfo();
